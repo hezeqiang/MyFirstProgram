b1b0ba9e2709110a3217b35cf719d46b74405217
he <56215142+hezeqiang@users.noreply.github.com>
2019-11-28 18:08:25 +0800

    update source1

---
+++ MyFirstProgram/source1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// 函数声明 
+
+
+void func(void);
+
+static int count1 = 10; /* 全局变量 */
+
+int main()
+{
+	while (count1--)
+	{
+		func();
+	}
+	return 0;
+}
+// 函数定义
+void func(void)
+{
+	static int i = 5; // 局部静态变量,一旦赋值后，赋值语句不在执行
+	i++;
+	cout << "变量 i 为 " << i;
+	cout << " , 变量 count 为 " << count1 << endl;
+}
