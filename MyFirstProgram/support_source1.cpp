4f3b6a1da7b445cf1aeb1c44797cf2df584daf43
he <56215142+hezeqiang@users.noreply.github.com>
2019-12-01 21:18:53 +0800

    source2 and defined function 2019.12.1

---
+++ MyFirstProgram/support_source1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+extern int count1;
+
+// 函数定义
+void func(void)
+{
+	static int i = 5; // 局部静态变量,一旦赋值后，赋值语句不再执行
+	i++;
+	cout << "变量 i 为 " << i;
+	cout << " , 变量 count 为 " << count1 << endl;
+}
